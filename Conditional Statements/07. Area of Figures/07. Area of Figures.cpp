61dd929ef743618cacba3a97600e18c0bd0695c0
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-01-13 11:43:12 +0200

    Commit 07. Area of Figures

---
+++ Conditional Statements/07. Area of Figures/07. Area of Figures.cpp
@@ -0,0 +1,45 @@
+// 07. Area of Figures.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+const double pi = 3.1415926535;
+
+int main()
+{
+	string figureType;
+	cin >> figureType;
+	double area = 0;
+
+	if (figureType == "square") {
+		double a;
+		cin >> a;
+		area = a * a;
+	}
+	else if (figureType == "rectangle") {
+		double a, b;
+		cin >> a >> b;
+		area = a * b;
+	}
+	else if (figureType == "circle") {
+		double r;
+		cin >> r;
+		area = pi * r * r;
+	}
+	else if (figureType == "triangle") {
+		double a, ha;
+		cin >> a >> ha;
+		area = (a * ha)/2;
+	}
+
+	cout.setf(ios::fixed);
+	cout.precision(3);
+	cout << area << endl;
+
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
