20d7f87e032c935337235f752833d3229a7a563f
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-01-27 10:39:04 +0200

    Commit tasks 01. Numbers from 1 to 100 & 02. Numbers N...1

---
+++ For Loop/01. Numbers from 1 to 100/01. Numbers from 1 to 100.cpp
@@ -0,0 +1,17 @@
+// 01. Numbers from 1 to 100.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 100; i++) {
+		cout << i << endl;
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
---
+++ For Loop/02. Numbers N-1/02. Numbers N-1.cpp
@@ -0,0 +1,20 @@
+// 02. Numbers N-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = n; i > 0; i--) {
+		cout << i << endl;
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
