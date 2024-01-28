ce5926b1e829477a203c504a11162a1fdd7081b1
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-01-28 10:17:19 +0200

    Commit 01. Numbers Ending in 7

---
+++ For Loop - Exercise/01. Numbers Ending in 7/01. Numbers Ending in 7.cpp
@@ -0,0 +1,17 @@
+// 01. Numbers Ending in 7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 7; i < 1000; i += 10) {
+		cout << i << endl;
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
