f88e0412cae3104c4edf86b9c61ed5357bc7f39e
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-02-10 10:59:19 +0200

    Commit 03. Combinations

--- Nested Loops - Lab/03. Combinations/03. Combinations.cpp
+++ Nested Loops - Lab/03. Combinations/03. Combinations.cpp
@@ -6,8 +6,27 @@ using namespace std;
 
 int main()
 {
+	int n;
+	cin >> n;
+	int count = 0;
 
+	for (int x1 = 0; x1 <= n; x1++)
+	{
+		for (int x2 = 0; x2 <= n; x2++)
+		{
+			for (int x3 = 0; x3 <= n; x3++)
+			{
+				int sum = x1 + x2 + x3;
+				if (sum == n) 
+				{
+					count++;
+				}
+			}
+		}
+	}
+	cout << count << endl;
 
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
